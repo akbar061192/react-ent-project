dc4baeb2b11d4297ae2e8e36c757d7eee143d967
Akbar Khan <akbarkhan061192@gmail.com>
2022-07-29 14:46:20 +0530

    code base

---
+++ src/components/Genres/Genres.js
@@ -0,0 +1,54 @@
+import { Chip } from '@material-ui/core';
+import axios from 'axios';
+import { useEffect } from 'react';
+
+const Genres = ({ selectedGenres, setSelectedGenres, genres, setGenres, type, setPage }) => {
+  const handleAdd = genre => {
+    setSelectedGenres([...selectedGenres, genre]);
+    setGenres(genres.filter(g => g.id !== genre.id));
+    setPage(1);
+  };
+
+  const handleRemove = genre => {
+    setSelectedGenres(selectedGenres.filter(selected => selected.id !== genre.id));
+    setGenres([...genres, genre]);
+    setPage(1);
+  };
+
+  const fetchGenres = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/genre/${type}/list?api_key=c72d30702ab3ffa8f3e95e24196b7ce5&language=en-US`
+    );
+    setGenres(data.genres);
+  };
+
+  useEffect(() => {
+    fetchGenres();
+
+    return () => {
+      setGenres({}); // unmounting
+    };
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div style={{ padding: '6px 0' }}>
+      {selectedGenres.map(genre => (
+        <Chip
+          style={{ margin: 2 }}
+          label={genre.name}
+          key={genre.id}
+          color='primary'
+          clickable
+          size='small'
+          onDelete={() => handleRemove(genre)}
+        />
+      ))}
+      {genres.map(genre => (
+        <Chip style={{ margin: 2 }} label={genre.name} key={genre.id} clickable size='small' onClick={() => handleAdd(genre)} />
+      ))}
+    </div>
+  );
+};
+
+export default Genres;
